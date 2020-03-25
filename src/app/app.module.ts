0176fa31f1543fe95380ce8a4293b9d0d5460842
Khushbu Raval <khushi29315@gmail.com>
2020-03-25 15:19:09 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,21 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 
+import { CustomerReducer } from './customer.reducer';
+import { CustomersViewComponent } from './customers-view/customers-view.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component'; 
+
 @NgModule({
   declarations: [
     AppComponent,
-    MyCounterComponent
+    MyCounterComponent,
+    CustomersViewComponent,
+    CustomerAddComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer })
+    StoreModule.forRoot({ count: counterReducer,customers: CustomerReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
